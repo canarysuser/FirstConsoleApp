6fbb49999ce919680dd6a3dbaeac5def791ff461
Sudhir <sudhir.nayak.b@gmail.com>
2025-08-06 13:03:37 +0530

    Sample Usage of Delegates and Events

--- FirstConsoleApp/WokringWithDelegates2.cs
+++ FirstConsoleApp/WokringWithDelegates2.cs
@@ -12,14 +12,14 @@ namespace FirstConsoleApp
         public int Number { get; set; }
         public string Message { get; set; }
         public CustomEventArgs(int number, string message) => (Number, Message) = (number, message);
-        
+
         public override string ToString() => $"[Number: {Number}, Message: {Message}]";
     }
 
     //Delegate for the custom event
     public delegate void CustomEventHandler(object sender, CustomEventArgs e);
 
-    public class  EventPublisher
+    public class EventPublisher
     {
         //Event declaration
         public CustomEventHandler CustomEvent;
@@ -31,9 +31,9 @@ namespace FirstConsoleApp
         }
         public void RaiseEvent()
         {
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                if(i % 4==0)
+                if (i % 4 == 0)
                     OnCustomEvent(i, $"Event raised with number {i}");
             }
         }
@@ -57,9 +57,63 @@ namespace FirstConsoleApp
             publisher.CustomEvent = new CustomEventHandler(subscriber.HandleCustomEvent);
             //Raise the event
             publisher.RaiseEvent();
-            
 
+            ParentClass parent = new ParentClass();
+            parent.AddItem(10);
+            parent.UpdateItem(0, 25);
+            parent.AddItem(20);
+            parent.UpdateItem(1, 25);
+            // parent.AddItem(30);
+        }
+    }
+    public class ParentClass
+    {
+        public int MaxItems = 50;
+        public List<DetailClass> Details { get; set; } = new List<DetailClass>();
+        public ParentClass()
+        {
 
         }
+        public void AddItem(int number)
+        {
+            DetailClass item = new DetailClass();
+            item.ItemAdded += ItemAdded;
+            item.StoredItems = number;
+            Details.Add(item);
+            Details.ForEach(d => Console.WriteLine($"Stored Items: {d.StoredItems}"));
+        }
+        public void UpdateItem(int index, int newValue)
+        {
+            Details[index].StoredItems = newValue;
+            Console.WriteLine($"Updated item at index {index} to {newValue}");
+        }
+        private void ItemAdded(object sender, DetailClass item)
+        {
+            if (Details.Sum(c=>c.StoredItems) < MaxItems)
+            {
+                Console.WriteLine($"Item added. Total items: {Details.Sum(c => c.StoredItems)}");
+            }
+            else
+            {
+                Console.WriteLine("Max items reached. Cannot add more.");
+            }
+        }
+    }
+    public delegate void ItemAddedHandler(object sender, DetailClass item);
+    public class DetailClass
+    {
+        public event ItemAddedHandler ItemAdded;
+        private int _storedItems;
+        public int StoredItems
+        {
+            get { return _storedItems; }
+            set
+            {
+                _storedItems = value;
+                ItemAdded?.Invoke(this, this);
+            }
+        }
+        
+
     }
 }
