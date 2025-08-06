9d51622cac558fa79253d3dde1f96fd37b89b7e1
Sudhir <sudhir.nayak.b@gmail.com>
2025-08-06 10:21:33 +0530

    Extension Methods added

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace FirstConsoleApp
 {
-    internal class Program
+    public static class ViewModelExtensions
+    {
+        public static bool IsValidEmail(this string input, string criteria)
+        {
+            return input.Contains(criteria);
+        }
+        public static bool DoWork(this Program program)
+        {
+            // Do some work here
+            return true;
+        }
+    }
+    /*class EmailString : String
+    {
+
+    }*/
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program.DoWork(); 
+
+            string email = "someone@example.com";
+            if (email.IsValidEmail("@"))
+            //if (Utitlity.IsValidEmail(email))
+            {
+                Console.WriteLine($"{email} is valid.");
+            } else
+                Console.WriteLine($"{email} is not valid.");
+
         }
     }
 }
