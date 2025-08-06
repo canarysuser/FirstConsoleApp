05cffa6d01feb75e07277ba49ce40912400fede8
Sudhir <sudhir.nayak.b@gmail.com>
2025-08-06 10:51:06 +0530

    Yield Statement

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -31,6 +31,46 @@
             } else
                 Console.WriteLine($"{email} is not valid.");
 
+            foreach(var name in GetNames())
+            {
+                Console.WriteLine($"{name}");
+            }
+            foreach (var power in Power(2, 10))
+            {
+                Console.WriteLine($"Power: {power}");
+            }
         }
+        static IEnumerable<int> Power(int num, int multiplier)
+        {
+            int result = 1;
+            for (int i = 0; i < multiplier; i++)
+            {
+                result *= num;
+                if(result <  200)
+                    yield return result;
+                else
+                {
+                     yield break; // Stop yielding if the result exceeds 200
+                }
+            }
+        }
+
+        // Yielding functions cannot be anonymous or lambda functions.
+        // They must be defined as a method with a return type of IEnumerable<T> or IEnumerator<T>, 
+        //IAsyncEnumerable<T>, or IAsyncEnumerator<T>, IEnumerable,IEnumerator.
+        // The yield return statement is used to return each element one at a time.
+        // Cannot use ref or out parameters with yield return.
+
+        static IEnumerable<string> GetNames()
+        {
+            yield return "Alice";           
+            Console.WriteLine("Returned Alice");
+            yield return "Bob";
+            Console.WriteLine("Returned Bob");
+            yield return "Charlie";
+            Console.WriteLine("Returned Charlie");
+        }
+
+        
     }
 }
